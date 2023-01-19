00bc48f4ec74ac79a5ff011320aff28636bfb8a0
Igor Vasiliev <igor_vasiliev@epam.com>
2023-01-20 01:27:34 +0400

    - first commit - frontend part for tin-dev app

--- next.config.js
+++ next.config.js
@@ -23,5 +23,6 @@ module.exports = () => {
                    };
                    return {
                      rewrites,
+                     nextConfig
                    };
                  };
