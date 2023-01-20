af71fa699c58957240e14c00a7478c44aa82b163
Igor Vasiliev <igor_vasiliev@epam.com>
2023-01-20 14:03:35 +0400

    - first commit - frontend part for tin-dev app

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
     return [
       {
         source: "/api",
-        destination: "/vacancies/pending",
+        destination: "https://tin-dev-back.herokuapp.com/api/users",
       },
     ];
   },
